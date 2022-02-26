535ac0b4508fba0b5973eb0663d606d79d38d5ec
Célestine Feuillat <celestine.feuillat@gmail.com>
2022-02-26 15:30:18 +0100

    Premières avancées

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function add(a, b) {
-    return a + b;
+    return Number(a) + Number(b);
 }
 
 function substract(a, b) {
@@ -16,14 +16,52 @@ function divide(a, b) {
 
 function operate(operator, a, b) {
     if (operator == '+') {
-        add(a, b);
+        return add(a, b);
     } else if (operator == '-') {
-        substract(a, b);
+        return substract(a, b);
     } else if (operator == 'x') {
-        multiply(a, b);
+        return multiply(a, b);
     } else if (operator == '/') {
-        divide(a, b);
+        return divide(a, b);
     }
 
 
 }
+
+const number = document.querySelectorAll(".number")
+const operator = document.querySelectorAll(".operator")
+const equal = document.querySelector("#equal")
+const screen = document.querySelector(".screen")
+const clear = document.querySelector("#clear")
+
+
+
+let displayValue = Number(screen.textContent);
+let operatorValue;
+let operation;
+
+number.forEach((num) => {
+    num.addEventListener('click', function() {
+        if (isNaN(screen.textContent)) {
+            screen.textContent = '';
+        }
+        screen.textContent += num.textContent;
+        displayValue = Number(screen.textContent);
+    })
+})
+
+operator.forEach((el) => {
+    el.addEventListener('click', function() {
+        operatorValue = el.textContent;
+        operation = [displayValue, el.textContent]
+        screen.textContent = el.textContent;
+        displayValue = '';
+    })
+})
+
+equal.addEventListener('click', function() {
+    screen.textContent = operate(operation[1], operation[0], displayValue);
+    displayValue = screen.textContent;
+})
+
+clear.addEventListener('click', () => { screen.textContent = '' })
